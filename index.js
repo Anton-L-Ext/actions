63d24ddbba8b71c8e72c775641712df012950552
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-12-12 08:59:13 +0100

    feat: Use ncc

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const core = require('@actions/core')
 const gihub = require('@actions/github')
 
-try {
-  const nameToGreet = core.getInput('who-to-greet')
-  const time = (new Date()).toDateString();
+async function run() {
+  try {
+    const nameToGreet = core.getInput('who-to-greet')
+    const time = (new Date()).toDateString();
 
-  console.log(`Hello ${nameToGreet}`)
-  core.setOutput('time', time);
+    console.log(`Hello ${nameToGreet}`)
+    core.setOutput('time', time);
 
-  const payload = JSON.stringify(gihub.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
+    const payload = JSON.stringify(gihub.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
+
+
+run();
